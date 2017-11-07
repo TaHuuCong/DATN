b260965d3b851bfd4af2b40c3a535aeff81ce9da
TAHUUCONG\Ta Huu Cong <cong.tsunajj310@gmail.com>
2017-11-07 23:14:11 +0700

    new 7/11

--- resources/views/pagination/paging.blade.php
+++ resources/views/pagination/paging.blade.php
@@ -1,24 +1,30 @@
 <?php
-    $link_limit = 6;
+    $link_limit = 4;
 ?>
 
 
 @if ($paginator->lastPage() > 1)
     <ul class="pagination">
-        <li class="{{ ($paginator->currentPage() == 1) ? ' disabled' : '' }}">
-            <a href="{{ $paginator->url(1) }}">First</a>
-        </li>
+        @if ($paginator->currentPage() != 1)
+            <li>
+                <a href="{{ $paginator->url(1) }}">Đầu</a>
+            </li>
+            <li>
+                <a href="{!! str_replace('/?','?',$paginator->url($paginator->currentPage() - 1)) !!}">&laquo;</a>
+            </li>
+        @endif
+
         @for ($i = 1; $i <= $paginator->lastPage(); $i++)
             <?php
-            $half_total_links = floor($link_limit / 2);
-            $from = $paginator->currentPage() - $half_total_links;
-            $to = $paginator->currentPage() + $half_total_links;
-            if ($paginator->currentPage() < $half_total_links) {
-               $to += $half_total_links - $paginator->currentPage();
-            }
-            if ($paginator->lastPage() - $paginator->currentPage() < $half_total_links) {
-                $from -= $half_total_links - ($paginator->lastPage() - $paginator->currentPage()) - 1;
-            }
+                $half_total_links = floor($link_limit / 2);
+                $from = $paginator->currentPage() - $half_total_links;
+                $to = $paginator->currentPage() + $half_total_links;
+                if ($paginator->currentPage() < $half_total_links) {
+                   $to += $half_total_links - $paginator->currentPage();
+                }
+                if ($paginator->lastPage() - $paginator->currentPage() < $half_total_links) {
+                    $from -= $half_total_links - ($paginator->lastPage() - $paginator->currentPage()) - 1;
+                }
             ?>
             @if ($from < $i && $i < $to)
                 <li class="{{ ($paginator->currentPage() == $i) ? ' active' : '' }}">
@@ -26,9 +32,15 @@
                 </li>
             @endif
         @endfor
-        <li class="{{ ($paginator->currentPage() == $paginator->lastPage()) ? ' disabled' : '' }}">
-            <a href="{{ $paginator->url($paginator->lastPage()) }}">Last</a>
-        </li>
+
+        @if ($paginator->currentPage() != $paginator->lastPage())
+            <li>
+                <a href="{!! str_replace('/?','?',$paginator->url($paginator->currentPage() + 1)) !!}">&raquo;</a>
+            </li>
+            <li>
+                <a href="{{ $paginator->url($paginator->lastPage()) }}">Cuối</a>
+            </li>
+        @endif
     </ul>
 @endif
 <script>
